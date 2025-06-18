75fc391362804463e264120d973a04fc1d316cfe
4BFC <4bee.code@gamil.com>
2025-06-18 16:26:03 +0900

    hotfix: image 삽입

--- client/app/components/layout/Header.tsx
+++ client/app/components/layout/Header.tsx
@@ -2,6 +2,11 @@ export default function Header() {
   return (
     <header className="text-2xl font-semibold min-h-[5rem] bg-background-primary">
       <div className="container h-full flex items-center">
+        <img
+          src="https://api-seapt-cdn-344342163.imgix.net/images/tr/tr-logo.png"
+          alt="logo"
+          className="w-20 h-20"
+        />
         <div className="py-2">
           <h1 className="text-2xl font-bold text-white">TR</h1>
           <span className="text-sub-1">TRANSFER STATION</span>
--- client/app/routes/shop.tsx
+++ client/app/routes/shop.tsx
@@ -12,6 +12,11 @@ export default function ShopLayout() {
     <div className="min-h-screen">
       <header className="text-2xl font-semibold min-h-[5rem] bg-white">
         <div className="container h-full flex items-center">
+          <img
+            src="https://api-seapt-cdn-344342163.imgix.net/images/tr/tr-logo.png"
+            alt="logo"
+            className="w-20 h-20"
+          />
           <div className="py-2">
             <h1 className="text-2xl font-bold text-background-primary">TR</h1>
             <span className="text-sub-1">TRANSFER STATION</span>
